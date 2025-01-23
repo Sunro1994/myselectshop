e449e3d9eafc5c86162ba49a6fd9eea8e540c2a4
Sunro1994 <gwangjulsr@gmail.com>
2025-01-23 22:38:55 +0900

    관심 상품 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductFolderRepository.java
@@ -1,7 +1,13 @@
 package com.sparta.myselectshop.repository;
 
+import com.sparta.myselectshop.entity.Folder;
+import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.entity.ProductFolder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+    Optional<ProductFolder> findByFolderAndProduct(Folder folder, Product product);
 }

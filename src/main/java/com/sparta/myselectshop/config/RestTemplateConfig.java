91a61780c22c72b39d4247a943a871ba2e4df31a
Sunro1994 <gwangjulsr@gmail.com>
2025-02-05 10:53:05 +0900

    Deprecated 클래스 수정

--- src/main/java/com/sparta/myselectshop/config/RestTemplateConfig.java
+++ src/main/java/com/sparta/myselectshop/config/RestTemplateConfig.java
@@ -14,8 +14,6 @@ public class RestTemplateConfig  {
         return restTemplateBuilder
                 // RestTemplate 으로 외부 API 호출 시 일정 시간이 지나도 응답이 없을 때
                 // 무한 대기 상태 방지를 위해 강제 종료 설정
-//                .setConnectTimeout(Duration.ofSeconds(10))
-//                .setReadTimeout(Duration.ofSeconds(10))
                 .connectTimeout(Duration.ofSeconds(5))
                 .readTimeout(Duration.ofSeconds(5))
                 .build();
